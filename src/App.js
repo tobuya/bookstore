7020eda063a717a16df88b9db3b141f3b4648867
tobuya <thomasobuya95@gmail.com>
2023-01-27 08:37:53 +0300

    Import react components to App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import BooksList from './components/BooksList';
+import CategoryPage from './components/CategoryPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<BooksList />} />
+          <Route path="/categories" element={<CategoryPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
