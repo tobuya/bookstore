2e68cd2d888b384e0196518120c971504f4f1e8f
tobuya <thomasobuya95@gmail.com>
2023-02-01 16:52:02 +0300

    Modify categoriesReducer

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,16 +1,13 @@
-// Action
 const CHECK_STATUS = 'bookstore/categories/CHECK_STATUS';
-
-// Initial State
 const initialState = { categories: [] };
 
-// Action creator
 export const checkStatus = () => ({ type: CHECK_STATUS });
 
-// Reducer
 const categoriesReducer = (state = initialState, action) => {
   if (action.type === CHECK_STATUS) {
-    return 'Under Construction';
+    return {
+      categories: [...state.categories, 'Under Construction'],
+    };
   }
   return state;
 };
