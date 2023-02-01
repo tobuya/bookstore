4c97654743dee7064bc39da76dc9d6f5ad4a4a6d
tobuya <thomasobuya95@gmail.com>
2023-02-01 16:58:55 +0300

    Dispatch an action for adding new books

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addBookAction } from '../redux/books/books';
 
-const BookForm = () => (
-  <section>
-    <h2>ADD NEW BOOK</h2>
-    <form>
-      <input type="text" placeholder="Book title" />
-      <input type="text" placeholder="Book author" />
-      <button type="submit">ADD BOOK</button>
-    </form>
-  </section>
-);
+const BookForm = () => {
+  const [bookState, setBookState] = useState({ id: '', title: '', author: '' });
+  const booksData = useSelector((state) => state.books);
+  const booksArray = booksData.books;
+  const bookId = booksArray.length;
+
+  const onAddingNewBook = (e) => {
+    setBookState(
+      { ...bookState, id: (bookId + 1).toString(), [e.target.name]: [e.target.value].toString() },
+    );
+  };
+
+  const dispatch = useDispatch();
+  const onNewBookSubmit = (e) => {
+    e.preventDefault();
+    dispatch(addBookAction(bookState));
+    setBookState({ id: '', title: '', author: '' });
+  };
+
+  return (
+    <section>
+      <h2>ADD NEW BOOK</h2>
+      <form onSubmit={onNewBookSubmit}>
+        <input type="text" name="title" value={bookState.title} onChange={onAddingNewBook} placeholder="Book title" />
+        <input type="text" name="author" value={bookState.author} onChange={onAddingNewBook} placeholder="Book author" />
+        <button type="submit">ADD BOOK</button>
+      </form>
+    </section>
+  );
+};
 
 export default BookForm;
