85738170e37ac1789714a8eb127cdd7029d469d9
tobuya <thomasobuya95@gmail.com>
2023-02-01 16:57:32 +0300

    Dispatch an action for removing a book

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBookAction } from '../redux/books/books';
 
 const Book = (props) => {
-  const { title, author } = props;
+  const { id, title, author } = props;
+
+  const dispatch = useDispatch();
+
+  const onRemovingBook = () => {
+    dispatch(removeBookAction(id));
+  };
 
   return (
     <li>
       <h3>{title}</h3>
       <p>{author}</p>
-      <button type="button">Delete</button>
+      <button type="button" onClick={onRemovingBook}>Delete</button>
     </li>
   );
 };
 
 Book.propTypes = {
+  id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
 };
