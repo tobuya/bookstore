bac764a76f0a4b15f8f9cc0d6d97ae335be33538
tobuya <thomasobuya95@gmail.com>
2023-02-03 18:24:52 +0300

    Refactor add book features to persist data in the server

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,24 +1,25 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { addBookAction } from '../redux/books/books';
+import { useDispatch } from 'react-redux';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { v4 } from 'uuid';
+import { addBook } from '../redux/books/books';
 
 const BookForm = () => {
-  const [bookState, setBookState] = useState({ id: '', title: '', author: '' });
-  const booksData = useSelector((state) => state.books);
-  const booksArray = booksData.books;
-  const bookId = booksArray.length;
+  const [bookState, setBookState] = useState({
+    id: '', title: '', author: '', category: '',
+  });
 
   const onAddingNewBook = (e) => {
     setBookState(
-      { ...bookState, id: (bookId + 1).toString(), [e.target.name]: [e.target.value].toString() },
+      { ...bookState, item_id: v4(), [e.target.name]: [e.target.value].toString() },
     );
   };
 
   const dispatch = useDispatch();
   const onNewBookSubmit = (e) => {
     e.preventDefault();
-    dispatch(addBookAction(bookState));
-    setBookState({ id: '', title: '', author: '' });
+    dispatch(addBook(bookState));
+    setBookState({ title: '', author: '', category: '' });
   };
 
   return (
@@ -27,6 +28,7 @@ const BookForm = () => {
       <form onSubmit={onNewBookSubmit}>
         <input type="text" name="title" value={bookState.title} onChange={onAddingNewBook} placeholder="Book title" />
         <input type="text" name="author" value={bookState.author} onChange={onAddingNewBook} placeholder="Book author" />
+        <input type="text" name="category" value={bookState.category} onChange={onAddingNewBook} placeholder="Book category" />
         <button type="submit">ADD BOOK</button>
       </form>
     </section>
