d13d2b7f3144175fc6dacb65b1a6f2475ef51af3
tobuya <thomasobuya95@gmail.com>
2023-02-04 18:57:21 +0300

    Add className to style Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { BsPersonFill } from 'react-icons/bs';
+import styles from '../styles/Navigation.module.css';
 
 const Navigation = () => (
   <nav>
-    <div>
-      <h1>Bookstore CMS</h1>
-      <ul>
-        <li>
-          <Link to="/">BOOKS</Link>
+    <div className={styles.container}>
+      <h1 className={styles.h}>Bookstore CMS</h1>
+      <ul className={styles.ul}>
+        <li className={styles.list}>
+          <Link to="/" className={styles.link}>BOOKS</Link>
         </li>
-        <li>
-          <Link to="categories">CATEGORIES</Link>
+        <li className={styles.list}>
+          <Link to="categories" className={styles.link}>CATEGORIES</Link>
         </li>
       </ul>
     </div>
-    <p>profile icon</p>
+    <BsPersonFill color="#0290ff" size={50} />
   </nav>
 );
 
